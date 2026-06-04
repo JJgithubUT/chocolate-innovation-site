bbbb80e3c757a7145401758cfd8519094bb6b6a0
Juan Javier <juanjcbreton@gmail.com>
2026-06-04 09:57:49 -0600

    First Commit | 4-6-2026 | main 1.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <main>
+      </main>
+    </div>
+  );
+}
+
+export default App;
